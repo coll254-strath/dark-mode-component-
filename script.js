bc8cbab4ce9538bd6d4d5f4b7534072fd5869346
Collins <149684399+coll254-strath@users.noreply.github.com>
2024-08-16 14:26:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
